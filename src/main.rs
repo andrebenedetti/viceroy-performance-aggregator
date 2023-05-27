88260b71686c6eb3009c804c953f017ebcd62006
drei <andredbenedetti@gmail.com>
2023-05-27 12:56:34 -0300

    add run time metrics

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::io::{self, BufRead};
 
-use regex::{Captures, Match, Regex};
+use regex::Regex;
 
 fn main() {
     let stdin = io::stdin();
-    let memory_regex = Regex::new(r"request completed using (.*)MB").unwrap();
+    let memory_regex = Regex::new(r"request completed using (.*) MB").unwrap();
+    let run_time_regex = Regex::new(r"request completed in (.*)(ms|µs)").unwrap();
 
     let mut ram_usage: Vec<f32> = Vec::new();
+    let mut run_time_us: Vec<f32> = Vec::new();
 
-    let max_lines = 20;
+    let max_lines = 200;
     let mut line_count = 0;
     for line in stdin.lock().lines() {
         line_count += 1;
@@ -16,7 +18,38 @@ fn main() {
 
         let captures = memory_regex.captures(line.as_str());
         match captures {
-            Some(v) => ram_usage.push(v.get(1).unwrap().as_str().parse().unwrap()),
+            Some(v) => {
+                match v.get(1) {
+                    Some(m) => {
+                        println!("{}", m.as_str());
+                        match m.as_str().parse::<f32>() {
+                            Ok(v) => {
+                                ram_usage.push(v);
+                                println!("parsing ok")
+                            }
+                            Err(e) => {
+                                println!("Parsed: {}", m.as_str());
+                                println!("{:?}", e)
+                            }
+                        }
+                    }
+                    None => (),
+                };
+            }
+            None => (),
+        }
+        let captures = run_time_regex.captures(line.as_str());
+        match captures {
+            Some(v) => {
+                let time_unit = v.get(2).unwrap().as_str();
+                let run_time = v.get(1).unwrap().as_str().parse().unwrap();
+
+                if time_unit == "ms" {
+                    run_time_us.push(run_time * 1000.0);
+                } else {
+                    run_time_us.push(run_time);
+                }
+            }
             None => (),
         }
 
@@ -26,4 +59,5 @@ fn main() {
     }
 
     println!("{:?}", ram_usage);
+    println!("{:?}", run_time_us);
 }
