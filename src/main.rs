c377cb19a46d381c2970d038bb4ee0ccfe0b4db8
drei <andredbenedetti@gmail.com>
2023-05-28 20:05:10 -0300

    report ram usage

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ fn main() {
     let (mut ram_usage, mut run_time_us) = log_reader::capture_measurements();
 
     run_time_us.sort_unstable();
+    ram_usage.sort_unstable();
     println!(
         "{}",
         measurements::aggregate_sorted_measurements(&run_time_us)
+    );
+    println!(
+        "{}",
+        measurements::aggregate_sorted_measurements(&ram_usage)
     );
 }
