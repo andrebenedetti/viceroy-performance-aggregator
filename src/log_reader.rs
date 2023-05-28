3108acba2cff66ade79f357f2177e597171e6a4a
drei <andredbenedetti@gmail.com>
2023-05-28 20:01:59 -0300

    refactor

--- src/log_reader.rs
+++ src/log_reader.rs
@@ -3,10 +3,24 @@ use std::io;
 use regex::Regex;
 use std::io::BufRead;
 
-pub fn read_stdin() -> (Vec<i32>, Vec<i32>) {
+fn parse_line(regex: &Regex, text: &str) -> Option<i32> {
+    if let Some(captures) = regex.captures(text) {
+        if let Some(m) = captures.get(1) {
+            match m.as_str().parse::<i32>() {
+                Ok(v) => return Some(v),
+                Err(_) => return None,
+            }
+        }
+    }
+
+    None
+}
+
+pub fn capture_measurements() -> (Vec<i32>, Vec<i32>) {
     let stdin = io::stdin();
     let memory_regex = Regex::new(r"request completed using (.*) MB").unwrap();
-    let run_time_regex = Regex::new(r"request completed in (.*)(ms|µs)").unwrap();
+    let run_time_regex_us = Regex::new(r"request completed in (.*)µs").unwrap();
+    let run_time_regex_ms = Regex::new(r"request completed in (.*)ms").unwrap();
 
     let mut ram_usage: Vec<i32> = Vec::new();
     let mut run_time_us: Vec<i32> = Vec::new();
@@ -17,30 +31,16 @@ pub fn read_stdin() -> (Vec<i32>, Vec<i32>) {
         line_count += 1;
         let line = line.expect("Could not read line from standard in");
 
-        if let Some(captures) = memory_regex.captures(line.as_str()) {
-            if let Some(m) = captures.get(1) {
-                match m.as_str().parse::<i32>() {
-                    Ok(v) => {
-                        ram_usage.push(v);
-                    }
-                    Err(_) => (),
-                }
-            }
+        if let Some(ram_datapoint) = parse_line(&memory_regex, line.as_str()) {
+            ram_usage.push(ram_datapoint);
         }
 
-        let captures = run_time_regex.captures(line.as_str());
-        match captures {
-            Some(v) => {
-                let time_unit = v.get(2).unwrap().as_str();
-                let run_time = v.get(1).unwrap().as_str().parse().unwrap();
+        if let Some(run_time_ms_datapoint) = parse_line(&run_time_regex_ms, line.as_str()) {
+            run_time_us.push(run_time_ms_datapoint * 1000);
+        }
 
-                if time_unit == "ms" {
-                    run_time_us.push(run_time * 1000);
-                } else {
-                    run_time_us.push(run_time);
-                }
-            }
-            None => (),
+        if let Some(run_time_us_datapoint) = parse_line(&run_time_regex_us, line.as_str()) {
+            run_time_us.push(run_time_us_datapoint);
         }
 
         if line_count >= max_lines {
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ pub mod log_reader;
 pub mod measurements;
 
 fn main() {
-    let (mut ram_usage, mut run_time_us) = log_reader::read_stdin();
+    let (mut ram_usage, mut run_time_us) = log_reader::capture_measurements();
 
     run_time_us.sort_unstable();
     println!(
